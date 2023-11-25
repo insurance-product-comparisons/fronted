689dbb0d17d2b73683864424e9e9cc79aa62ad1f
aumetros <aum3tros@yandex.ru>
2023-11-25 14:14:04 +0300

    chore(app): настройка сборки, создана архитектура приложения

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div className="App">
+			<h1>Платформа для сравнения страховых продуктов.</h1>
+			{/* @TODO: Создаем приложение */}
+		</div>
+	);
+}
+
+export default App;
