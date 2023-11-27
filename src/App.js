4d939a4a71e7d911dc4cfc235c810b135f9d1594
aumetros <aum3tros@yandex.ru>
2023-11-27 20:12:04 +0300

    feat(app): создан компонент и роут для ошибки 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Main, Osago, Kasko, Dms, Request } from 'pages';
+import { Main, Osago, Kasko, Dms, Request, NotFound } from 'pages';
 
 function App() {
 	/* @TODO: Создаем глобальные стейты приложения */
@@ -12,6 +12,7 @@ function App() {
 			<Route path="/request" element={<Request />} />
 			{/* <Route path="/products/:id" element={<Product />} /> Просмотр предложения страхованя */}
 			{/* <Route path="/admin" element={<Admin />} /> Панель администратора */}
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import { Osago } from './Osago';
 import { Kasko } from './Kasko';
 import { Dms } from './Dms';
 import { Request } from './Request';
+import { NotFound } from './NotFound';
 
-export { Main, Osago, Kasko, Dms, Request };
+export { Main, Osago, Kasko, Dms, Request, NotFound };
