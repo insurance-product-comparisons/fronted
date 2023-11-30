84e9bc685334e7097c6b03191ccce3d44da69c0e
aumetros <aum3tros@yandex.ru>
2023-11-30 22:31:43 +0300

    feat(pages): заведены истории страниц

---
+++ src/pages/Dms/Dms.stories.js
@@ -0,0 +1,10 @@
+import Dms from '.';
+
+export default {
+	title: 'Pages/DMS',
+	component: Dms,
+};
+
+export const Default = {
+	render: () => <Dms />,
+};
---
+++ src/pages/Kasko/Kasko.stories.js
@@ -0,0 +1,10 @@
+import Kasko from '.';
+
+export default {
+	title: 'Pages/KASKO',
+	component: Kasko,
+};
+
+export const Default = {
+	render: () => <Kasko />,
+};
