513626aa3d9eb24cc17eac0787cd1cafc0f1484c
aumetros <aum3tros@yandex.ru>
2023-12-01 16:18:06 +0300

    fix(ui): добавлен индексный файл для ui-kit

--- src/shared/ui/index.js
+++ src/shared/ui/index.js
@@ -1,1 +1,19 @@
 import Button from './Button';
+import Checkbox from './Checkbox';
+import Input from './Input';
+import Preloader from './Preloader';
+import RadioBtn from './RadioBtn';
+import TextArea from './TextArea';
+import Tooltip from './Tooltip';
+import Typography from './Typography';
+
+export {
+	Button,
+	Checkbox,
+	Input,
+	Preloader,
+	RadioBtn,
+	TextArea,
+	Tooltip,
+	Typography,
+};
