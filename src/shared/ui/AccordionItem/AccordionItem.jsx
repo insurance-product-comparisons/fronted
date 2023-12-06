17ba44156315729964ce0ea276edc46d4cf8f53a
aumetros <aum3tros@yandex.ru>
2023-12-06 23:51:08 +0300

    feat(ui): стилизован компонент AccordionItem

--- src/shared/ui/AccordionItem/AccordionItem.jsx
+++ src/shared/ui/AccordionItem/AccordionItem.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import styles from './AccordionItem.module.scss';
 
-function AccordionItem({ id, activeId, title, content, onClick }) {
+function AccordionItem({ id, activeId, title, content, onClick, mode }) {
 	const isActive = React.useMemo(() => id === activeId, [id, activeId]);
 
 	return (
-		<div className="accordion-item">
-			<div className="accordion-title" onClick={() => onClick(id)}>
+		<div className={styles[`type_${mode}`]}>
+			<div className={styles[`title_type_${mode}`]} onClick={() => onClick(id)}>
 				<div>{title}</div>
-				<div>{isActive ? '-' : '+'}</div>
+				<div
+					className={[
+						styles[`arrow_type_${mode}`],
+						styles[isActive ? 'open' : 'close'],
+					].join(' ')}
+				></div>
 			</div>
-			{isActive && <div className="accordion-content">{content}</div>}
+			<div
+				className={[
+					styles[`content_type_${mode}`],
+					styles[isActive ? 'open' : 'close'],
+				].join(' ')}
+			>
+				{content}
+			</div>
 		</div>
 	);
 }
