d4cd980d3e68d07617126dcf64dac4fb74c1d845
Daria Bykonya <dariabykonya@yandex.ru>
2023-12-13 12:37:19 +0100

    chore: husky

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -4,23 +4,20 @@ import { LinkRouter } from 'shared/ui';
 import { Typography } from 'shared/ui';
 
 function NavigationBar({ data, mode, variant, color }) {
+	const linkItems = data.map((item) => (
+		<LinkRouter
+			key={item.id}
+			route={item.route}
+			text={
+				<Typography variant={variant} color={color}>
+					{item.text}
+				</Typography>
+			}
+			mode="default"
+		/>
+	));
 
-   const linkItems = data.map((item) => (
-      <LinkRouter
-         key={item.id}
-         route={item.route}
-         text={
-            <Typography variant={variant} color={color}>
-               {item.text}
-            </Typography>
-         }
-         mode='default' 
-      />
-   ));
-
-   return (
-      <List children={linkItems} mode={mode} />
-   )
+	return <List children={linkItems} mode={mode} />;
 }
 
-export default NavigationBar
+export default NavigationBar;
