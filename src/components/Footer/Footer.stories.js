47068d4658ed4821da795b6de88372bb1e514587
aumetros <aum3tros@yandex.ru>
2023-12-14 20:10:54 +0300

    fix(footer): изменена ошибка в истории Footer

--- src/components/Footer/Footer.stories.js
+++ src/components/Footer/Footer.stories.js
@@ -5,35 +5,6 @@ export default {
 	title: 'Components/Footer',
 	component: Footer,
 	decorators: [withRouter],
-	argTypes: {
-		Logo: {
-			description: 'Логотип',
-			control: {
-				type: 'file',
-				accept: '.png',
-			},
-		},
-		Messenger: {
-			description: 'Изображение мессенджера',
-			control: {
-				type: 'file',
-				accept: '.png',
-			},
-		},
-		Height: {
-			type: 'string',
-			description: 'Высота подвала сайта',
-			control: 'text',
-		},
-		BacgroundColor: {
-			type: 'string',
-			description: 'Цвет фона',
-			defaultValue: '',
-			control: {
-				type: 'color',
-			},
-		},
-	},
 };
 
 const Template = (arg) => <Footer {...arg} />;
