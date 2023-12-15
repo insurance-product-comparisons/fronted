c6e51dcde013725a13e9bdffcd0dc21a20a5b87f
Ksenia Terekhova <kse.terekhova@gmail.com>
2023-12-15 17:45:03 +0300

    fix: добавлена форма заявкм на консультацию

---
+++ src/components/Form/Form.stories.js
@@ -0,0 +1,21 @@
+import Form from '.';
+
+export default {
+	title: 'Components/Form',
+	component: Form,
+	tags: ['autodocs'],
+	argTypes: {
+		children: {
+			type: 'string',
+			description: 'Задает наименование формы',
+			control: 'text',
+		},
+	},
+};
+
+const Template = (arg) => <Form {...arg} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	children: 'Форма',
+};
