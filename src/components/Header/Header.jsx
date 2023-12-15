2caae7910ab634d896eba97c18a77069fd4d148b
aumetros <aum3tros@yandex.ru>
2023-12-15 19:25:24 +0300

    fix: общий рефакторинг

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import styles from './Header.module.scss';
 import Logo from 'shared/ui/Logo';
 import Button from 'shared/ui/Button';
-import NavigationBar from 'components/NavigationBar';
+import { NavigationBar, Section } from 'components';
 import Input from 'shared/ui/Input';
 import { useState } from 'react';
 import cn from 'classnames';
@@ -10,7 +10,7 @@ export function Header() {
 	const [isSearchOpen, setIsSearchOpen] = useState(false);
 	const [searchValue, setSearchValue] = useState('');
 
-	const headerClass = cn(styles.root, {
+	const headerClass = cn(styles.menu, {
 		[styles['header-search']]: isSearchOpen,
 	});
 
@@ -37,59 +37,63 @@ export function Header() {
 	];
 
 	return (
-		<header className={headerClass}>
-			<Logo mode="main" />
+		<header className={styles.root}>
+			<Section>
+				<div className={headerClass}>
+					<Logo mode="main" />
 
-			{!isSearchOpen && (
-				<NavigationBar
-					data={arrLinks}
-					mode="header"
-					variant="body1"
-					color="black"
-				/>
-			)}
-
-			<div className={styles['buttons']}>
-				<div className={styles['search-block']}>
 					{!isSearchOpen && (
-						<button
-							className={styles['button-search']}
-							onClick={openSearch}
-						></button>
+						<NavigationBar
+							data={arrLinks}
+							mode="header"
+							variant="body1"
+							color="black"
+						/>
 					)}
 
-					{isSearchOpen && (
-						<div className={styles['search-input']}>
-							<div className={styles['icon-search']}></div>
-							<Input
-								name="search"
-								type="search"
-								placeholder="Поиск по сайту"
-								id="search"
-								mode="default"
-								submode="header"
-								isValid={true}
-								isDisabled={false}
-								value={searchValue}
-								onChange={handleInputChange}
-							/>
-							<button
-								className={styles['icon-close']}
-								onClick={closeSearch}
-							></button>
+					<div className={styles['buttons']}>
+						<div className={styles['search-block']}>
+							{!isSearchOpen && (
+								<button
+									className={styles['button-search']}
+									onClick={openSearch}
+								></button>
+							)}
+
+							{isSearchOpen && (
+								<div className={styles['search-input']}>
+									<div className={styles['icon-search']}></div>
+									<Input
+										name="search"
+										type="search"
+										placeholder="Поиск по сайту"
+										id="search"
+										mode="default"
+										submode="header"
+										isValid={true}
+										isDisabled={false}
+										value={searchValue}
+										onChange={handleInputChange}
+									/>
+									<button
+										className={styles['icon-close']}
+										onClick={closeSearch}
+									></button>
+								</div>
+							)}
 						</div>
-					)}
+						<Button
+							type="Button"
+							bgcolor="ghost"
+							mode="enter"
+							disabled={false}
+							onClick
+						>
+							Войти
+						</Button>
+					</div>
 				</div>
-				<Button
-					type="Button"
-					bgcolor="ghost"
-					mode="enter"
-					disabled={false}
-					onClick
-				>
-					Войти
-				</Button>
-			</div>
+			</Section>
 		</header>
 	);
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,20 @@
+import { ProductContainer, ConsultForm, AnswersList } from 'components';
+import cardItems from 'shared/utils/constants/cardItems';
+import { answersMain } from 'shared/store/answersMain';
 import styles from './Main.module.scss';
 
 function Main() {
 	return (
 		<>
-			<main className={styles.root}></main>
+			<main className={styles.root}>
+				<ProductContainer data={cardItems} mode="default" />
+				<ConsultForm />
+				<AnswersList
+					title="Вопросы и ответы"
+					data={answersMain}
+					mode="default"
+				/>
+			</main>
 		</>
 	);
 }
