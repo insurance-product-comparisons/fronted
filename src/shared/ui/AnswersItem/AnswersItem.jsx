8416470a847150fafc33631a316c12149378dda0
aumetros <aum3tros@yandex.ru>
2023-12-18 18:18:32 +0300

    refactor(answersitem): переменные модов перенесены в отдельный файл

--- src/shared/ui/AnswersItem/AnswersItem.jsx
+++ src/shared/ui/AnswersItem/AnswersItem.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Typography } from 'shared/ui';
+import { DEFAULT } from 'shared/utils/constants/modes';
 import styles from './AnswersItem.module.scss';
 
 function AnswersItem({ data, mode }) {
 	const [isOpen, setIsOpen] = React.useState(false);
+	const componentMode = mode ? mode : DEFAULT;
 
-	const contentClassName = classNames(styles[`content-${mode}`], {
+	const componentClassName = classNames(styles[componentMode], {
 		[styles.open]: isOpen,
 	});
-	const arrowClassName = classNames(styles[`arrow-${mode}`], {
+	const contentClassName = classNames(styles[`content-${componentMode}`], {
+		[styles.open]: isOpen,
+	});
+	const arrowClassName = classNames(styles[`arrow-${componentMode}`], {
 		[styles.rotate]: isOpen,
 	});
 
 	return (
-		<div className={styles[mode]}>
-			<div className={styles[`wrapper-${mode}`]}>
+		<div className={componentClassName}>
+			<div className={styles[`wrapper-${componentMode}`]}>
 				<Typography
 					variant="body1"
 					style={{ cursor: 'pointer' }}
