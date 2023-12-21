b84c2d9cdfa719fb6b67c484707c927332f965f7
aumetros <aum3tros@yandex.ru>
2023-12-21 20:11:47 +0300

    feat(notfound): создан компонент страницы 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,20 @@
+import { Section } from 'components';
+import { Typography } from 'shared/ui';
+import styles from './NotFound.module.scss';
+
 function NotFound() {
-	return <></>;
+	return (
+		<main className={styles.root}>
+			<Section>
+				<div className={styles.container}>
+					<Typography variant={'h5'}>
+						<span className={styles.title}>404</span>
+					</Typography>
+					<Typography variant={'h4'}>Этой страницы не существует.</Typography>
+				</div>
+			</Section>
+		</main>
+	);
 }
 
 export default NotFound;
