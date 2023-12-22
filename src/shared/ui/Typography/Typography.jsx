a0fe859eddfe64f29bfc12c0a3765ad48e4574f2
Ksenia Terekhova <kse.terekhova@gmail.com>
2023-12-22 14:38:01 +0300

    feat: добавлена страница Каско

--- src/shared/ui/Typography/Typography.jsx
+++ src/shared/ui/Typography/Typography.jsx
@@ -3,13 +3,14 @@ import cn from 'classnames';
 import { VARIANTS_MAPPING } from 'shared/utils/constants/typography_var';
 import styles from './Typography.module.scss';
 
-function Typography({ variant, color, children, ...args }) {
+function Typography({ variant, color, children, padding, ...args }) {
 	const Component = variant ? VARIANTS_MAPPING[variant] : 'p';
 	return (
 		<Component
 			className={cn({
 				[styles[`typography--variant-${variant}`]]: variant,
 				[styles[`typography--color-${color}`]]: color,
+				[styles[`typography--padding-${padding}`]]: padding,
 			})}
 			{...args}
 		>
