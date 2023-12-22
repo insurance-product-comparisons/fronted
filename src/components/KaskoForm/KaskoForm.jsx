77c5a0fa676ab57d16d2cacff4db5d356ba86a99
aumetros <aum3tros@yandex.ru>
2023-12-23 00:01:56 +0300

    fix(kaskoform): исправлена ошибка регистрации инпута

--- src/components/KaskoForm/KaskoForm.jsx
+++ src/components/KaskoForm/KaskoForm.jsx
@@ -1,8 +1,16 @@
 import Form from 'components/Form';
 import { Button, FormInput } from 'shared/ui';
+import { useForm } from 'react-hook-form';
 import styles from './KaskoFrom.module.scss';
 
 function KaskoForm() {
+	const {
+		register,
+		formState: { isValid },
+	} = useForm({
+		mode: 'onChange',
+	});
+
 	return (
 		<div className={styles.wrapper}>
 			<Form type="kasko-form">
@@ -11,23 +19,45 @@ function KaskoForm() {
 						select
 						textLabel="Марка автомобиля"
 						submode="select-kasko-short"
+						name="autoMark"
+						id="autoMark"
 					/>
 					<FormInput
 						select
 						textLabel="Модель автомобиля"
 						submode="select-kasko-short"
+						name="autoModel"
+						id="autoModel"
 					/>
 				</div>
-				<FormInput select textLabel="Стаж вождения" submode="select-kasko" />
-				<FormInput textLabel="Госномер" mode="kasko" />
-				<FormInput textLabel="Год выпуска" mode="kasko" />
+				<FormInput
+					select
+					textLabel="Стаж вождения"
+					submode="select-kasko"
+					name="experience"
+					id="experience"
+				/>
+				<FormInput
+					textLabel="Госномер"
+					mode="kasko"
+					name="carNumber"
+					id="carNumber"
+					register={register}
+				/>
+				<FormInput
+					textLabel="Год выпуска"
+					mode="kasko"
+					name="year"
+					id="year"
+					register={register}
+				/>
 				<div className={styles.btn}>
 					<Button
 						children="Рассчитать"
 						type="submit"
 						bgcolor="accent"
 						mode="count"
-						disabled
+						disabled={!isValid}
 					/>
 				</div>
 			</Form>
