c631b397c53f7facc8c3e11c2b94337db6a88591
Daria Bykonya <dariabykonya@yandex.ru>
2023-12-25 11:44:55 +0100

    fix: добавлена анимация для SuccessfulRequest на странице Request

--- src/pages/Request/Request.jsx
+++ src/pages/Request/Request.jsx
@@ -1,5 +1,6 @@
 import { Section, SuccessfulRequest, UnusualCaseForm } from 'components';
 import { useState } from 'react';
+import styles from './Request.module.scss';
 
 function Request() {
 	const [formSubmitted, setFormSubmitted] = useState(false);
@@ -11,15 +12,26 @@ function Request() {
 	return (
 		<main>
 			<Section>
-				{!formSubmitted ? (
-					<UnusualCaseForm onSubmit={handleFormSubmit} />
-				) : (
-					<SuccessfulRequest
-						title="Заявка на ситуацию, которой нет в каталоге"
-						type="unusual-case"
-						text="Подтверждение придет вам на электронную почту."
-					/>
-				)}
+				<div
+					className={`${styles.formWrapper} ${
+						formSubmitted ? styles.invisibly : ''
+					}`}
+				>
+					{!formSubmitted && <UnusualCaseForm onSubmit={handleFormSubmit} />}
+				</div>
+				<div
+					className={`${styles.requestWrapper} ${
+						formSubmitted ? styles.visibly : styles.invisibly
+					}`}
+				>
+					{formSubmitted && (
+						<SuccessfulRequest
+							title="Заявка на ситуацию, которой нет в каталоге"
+							type="unusual-case"
+							text="Подтверждение придет вам на электронную почту."
+						/>
+					)}
+				</div>
 			</Section>
 		</main>
 	);
