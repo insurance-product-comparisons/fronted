fd4b68326dd276b2d1d03edddb3a4b1101ddc13d
aumetros <aum3tros@yandex.ru>
2024-01-10 22:01:31 +0300

    fix(osagoform): верстка по pixel perfect

--- src/components/OsagoForm/OsagoForm.jsx
+++ src/components/OsagoForm/OsagoForm.jsx
@@ -6,24 +6,36 @@ function OsagoForm() {
 	return (
 		<div className={styles.wrapper}>
 			<Form type="osago-form">
-				<FormInput select textLabel="Тип ТС" submode="select-osago" />
-				<FormInput
-					select
-					textLabel="Мощность двигателя"
-					submode="select-osago"
-				/>
-				<div className={styles[`container-inputs`]}>
+				<fieldset className={styles.fieldset}>
+					<FormInput select textLabel="Тип ТС" submode="select-osago" />
+					<FormInput
+						select
+						textLabel="Мощность двигателя"
+						submode="select-osago"
+					/>
+				</fieldset>
+				<fieldset className={styles.fieldset}>
 					<FormInput select textLabel="Регион" submode="select-osago" />
 					<FormInput select textLabel="Район" submode="select-osago" />
-				</div>
-				<FormInput select textLabel="Число водителей" submode="select-osago" />
-				<FormInput select textLabel="Возраст водителя" submode="select-osago" />
-				<FormInput select textLabel="Стаж водителя" submode="select-osago" />
-				<FormInput
-					select
-					textLabel="Коэффициент «Бонус-Малус»"
-					submode="select-osago"
-				/>
+				</fieldset>
+				<fieldset className={styles.fieldset}>
+					<FormInput
+						select
+						textLabel="Число водителей"
+						submode="select-osago"
+					/>
+					<FormInput
+						select
+						textLabel="Возраст водителя"
+						submode="select-osago"
+					/>
+					<FormInput select textLabel="Стаж водителя" submode="select-osago" />
+					<FormInput
+						select
+						textLabel="Коэффициент «Бонус-Малус»"
+						submode="select-osago"
+					/>
+				</fieldset>
 				<div className={styles[`container-btn`]}>
 					<LinkComponent
 						link="https://autoins.ru/osago/raschet-stoimosti-osago/proverit-kbm/"
