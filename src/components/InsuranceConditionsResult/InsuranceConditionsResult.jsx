14357f1e9763c2eb4ba9eccd1d712f26802c2b71
aumetros <aum3tros@yandex.ru>
2024-01-17 19:11:38 +0300

    refactor(insuranceresultcard): валюта указана с использованием псевдоэлемента

--- src/components/InsuranceConditionsResult/InsuranceConditionsResult.jsx
+++ src/components/InsuranceConditionsResult/InsuranceConditionsResult.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { REG_VALUE, REG_SPACE } from 'shared/utils/constants/regEx';
 import styles from './InsuranceConditionsResult.module.scss';
 
 import { Typography } from 'shared/ui';
@@ -8,15 +9,26 @@ function InsuranceConditionsResult({ data }) {
 		<div className={styles.container} key={data.id}>
 			<div className={styles['conditions-item']}>
 				<Typography variant={'body2'}>{data.franchise.name}</Typography>
-				<Typography variant={'body1'}>{data.franchise.sum}</Typography>
+
+				<Typography variant={'body1'}>
+					<span className={styles['value-container']}>
+						{data.franchise.sum.replace(REG_VALUE, REG_SPACE)}
+					</span>
+				</Typography>
 			</div>
 			<div className={styles['conditions-item']}>
 				<Typography variant={'body2'}>{data.period.name}</Typography>
-				<Typography variant={'body1'}>{data.period.value}</Typography>
+				<Typography variant={'body1'}>
+					<span className={styles['year-container']}>{data.period.value}</span>
+				</Typography>
 			</div>
 			<div className={styles['conditions-item']}>
 				<Typography variant={'body2'}>{data.price.name}</Typography>
-				<Typography variant={'body1'}>{data.price.value}</Typography>
+				<Typography variant={'body1'}>
+					<span className={styles['value-container']}>
+						{data.price.value.replace(REG_VALUE, REG_SPACE)}
+					</span>
+				</Typography>
 			</div>
 		</div>
 	);
