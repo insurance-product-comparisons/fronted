861e53d47e8f587aa3f0f8ba6cbddf91000380f6
aumetros <aum3tros@yandex.ru>
2024-01-26 17:24:26 +0300

    feat(resultcard): реализована мобильная версия карточки

--- src/components/InsuranceConditionsResult/InsuranceConditionsResult.jsx
+++ src/components/InsuranceConditionsResult/InsuranceConditionsResult.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { REG_VALUE, REG_SPACE } from 'shared/utils/constants/regEx';
 import cn from 'classnames';
+import useWidth from 'shared/hooks/useWidth';
 import styles from './InsuranceConditionsResult.module.scss';
 
 import { Typography } from 'shared/ui';
 
 function InsuranceConditionsResult({ data, page, font }) {
+	const windowWidth = useWidth();
+
 	return (
 		<div className={cn(styles.container, { [styles[page]]: page })}>
 			<div className={cn(styles['conditions-item'], { [styles[page]]: page })}>
 				<Typography variant={font || 'body2'}>
 					{data ? data.franchise.name : '-'}
 				</Typography>
-				<Typography variant={'body1'}>
+				<Typography variant={windowWidth < 1023 ? 'mob1' : 'body1'}>
 					<span className={styles['value-container']}>
 						{data ? data.franchise.sum.replace(REG_VALUE, REG_SPACE) : '-'}
 					</span>
@@ -22,7 +25,7 @@ function InsuranceConditionsResult({ data, page, font }) {
 				<Typography variant={font || 'body2'}>
 					{data ? data.period.name : '-'}
 				</Typography>
-				<Typography variant={'body1'}>
+				<Typography variant={windowWidth < 1023 ? 'mob1' : 'body1'}>
 					<span className={styles['year-container']}>
 						{data ? data.period.value : '-'}
 					</span>
@@ -32,7 +35,7 @@ function InsuranceConditionsResult({ data, page, font }) {
 				<Typography variant={font || 'body2'}>
 					{data ? data.price.name : '-'}
 				</Typography>
-				<Typography variant={'body1'}>
+				<Typography variant={windowWidth < 1023 ? 'mob1' : 'body1'}>
 					<span className={styles['value-container']}>
 						{data ? data.price.value.replace(REG_VALUE, REG_SPACE) : '-'}
 					</span>
